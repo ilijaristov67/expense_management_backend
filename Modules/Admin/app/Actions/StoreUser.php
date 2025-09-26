292902ec6675efde5aaba02343f30c92f973dc20
ilijaristov67 <ilijaristov67@gmail.com>
2025-09-26 23:15:21 +0200

    someir

---
+++ Modules/Admin/app/Actions/StoreUser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Admin\Actions;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class StoreUser
+{
+    use AsAction;
+
+    public function handle()
+    {
+        dd('here');
+    }
+}
